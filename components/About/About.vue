0ad6a2567205c5d1c04a34faf10f0379721cd3fe
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-09-27 12:28:46 +0200

    refactor

---
+++ components/About/About.vue
@@ -0,0 +1,95 @@
+<template>
+  <article class="about">
+
+    <section class="about__intro">
+
+      <h2 class="about__intro-title">
+        — ABOUT DIWANEE
+      </h2>
+
+      <p class="about__intro-content">
+        Diwanee, is a digital media
+        company that specializes in
+        content creation targeting
+        women in the Middle East.
+      </p>
+
+      <div class="about__intro-card about__intro-card--left">
+        <div class="about__intro-card__text">
+          <h4>
+            Diwanee partners with YouTube
+          </h4>
+          <p>
+            To create the only regional female multi channel network called “Duniati”.
+          </p>
+        </div>
+      </div>
+
+      <div class="about__intro-card about__intro-card--right">
+        <div class="about__intro-card__text about__intro-card--right__text">
+          <h4>
+            Diwanee partners with YouTube
+          </h4>
+          <p>
+            To create the only regional female multi channel network called “Duniati”.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section class="about__jumbotron">
+
+      <h2 class="about__jumbotron-title">
+        — DIGITAL BIG - THINKERS
+      </h2>
+      <p class="about__jumbotron-content">
+
+        Our most prized technological accomplishment is a modern media stack platform, named Harmony, created with a lot of passion and knowledge. The platform was built from ground up, for digital-native authoring, publishing, advertising and distribution. We made Harmony the core of our business, so that we could distribute content to our readers on any device and on any platform.
+      </p>
+
+    </section>
+
+    <section class="about__stats">
+
+      <h2 class="about__stats-title">
+        — OUR AUDIENCE
+      </h2>
+
+      <div class="about__stats__items">
+        <about-cards>
+          <template slot="numbers">6.</template>
+          <template slot="text">Media Brands</template>
+        </about-cards>
+
+        <about-cards>
+          <template slot="numbers">8.</template>
+          <template slot="text">Milion Monthly Unique Users</template>
+        </about-cards>
+
+        <about-cards>
+          <template slot="numbers">29.</template>
+          <template slot="text">Milion Page Views</template>
+        </about-cards>
+
+        <about-cards>
+          <template slot="numbers">38.</template>
+          <template slot="text">Milion Fans Across Social Channels</template>
+        </about-cards>
+      </div>
+
+    </section>
+
+  </article>
+</template>
+
+<script>
+import AboutCards from "~/components/About/AboutCards.vue";
+export default {
+  components: {
+    AboutCards
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,107 +1,26 @@
 <template>
   <div>
 
-    <!-- Vasina Igra -->
+    <!-- Vasyly Game -->
     <h1 class="title">
       we are building the future of digital media
     </h1>
     <logo class="vue-logo" />
     <!-- Ende -->
 
-    <article class="about">
-
-      <section class="about__intro">
-
-        <h2 class="about__intro-title">
-          — ABOUT DIWANEE
-        </h2>
-
-        <p class="about__intro-content">
-          Diwanee, is a digital media
-          company that specializes in
-          content creation targeting
-          women in the Middle East.
-        </p>
-
-        <div class="about__intro-card about__intro-card--left">
-          <div class="about__intro-card__text">
-            <h4>
-              Diwanee partners with YouTube
-            </h4>
-            <p>
-              To create the only regional female multi channel network called “Duniati”.
-            </p>
-          </div>
-        </div>
-
-        <div class="about__intro-card about__intro-card--right">
-          <div class="about__intro-card__text about__intro-card--right__text">
-            <h4>
-              Diwanee partners with YouTube
-            </h4>
-            <p>
-              To create the only regional female multi channel network called “Duniati”.
-            </p>
-          </div>
-        </div>
-
-      </section>
-
-      <section class="about__jumbotron">
-
-        <h2 class="about__jumbotron-title">
-          — DIGITAL BIG - THINKERS
-        </h2>
-        <p class="about__jumbotron-content">
-
-          Our most prized technological accomplishment is a modern media stack platform, named Harmony, created with a lot of passion and knowledge. The platform was built from ground up, for digital-native authoring, publishing, advertising and distribution. We made Harmony the core of our business, so that we could distribute content to our readers on any device and on any platform.
-        </p>
-
-      </section>
-
-      <section class="about__stats">
-
-        <h2 class="about__stats-title">
-          — OUR AUDIENCE
-        </h2>
-
-        <div class="about__stats__items">
-          <app-stats-numbers>
-            <h3 slot="numbers">6.</h3>
-            <p slot="text">Media Brands</p>
-          </app-stats-numbers>
-
-          <app-stats-numbers>
-            <h3 slot="numbers">8.</h3>
-            <p slot="text">Milion Monthly Unique Users</p>
-          </app-stats-numbers>
-
-          <app-stats-numbers>
-            <h3 slot="numbers">29.</h3>
-            <p slot="text">Milion Page Views</p>
-          </app-stats-numbers>
-
-          <app-stats-numbers>
-            <h3 slot="numbers">38.</h3>
-            <p slot="text">Milion Fans Across Social Channels</p>
-          </app-stats-numbers>
-        </div>
-
-      </section>
-
-    </article>
+    <About />
 
   </div>
 </template>
 
 <script>
 import Logo from "~/components/Logo.vue";
-import AppStatsNumbers from "~/components/AppStatsNumbers.vue";
+import About from "~/components/About/About.vue";
 
 export default {
   components: {
     Logo,
-    AppStatsNumbers
+    About
   }
 };
 </script>

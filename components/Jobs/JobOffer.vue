7d82159d4a7576f7b783f4cad3b12b9b57dcbd23
Veljko Blagojevic <veljkod.blagojevic@gmail.com>
2018-09-27 22:25:46 +0200

    finished job offer

---
+++ components/Jobs/JobOffer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="jobs__list__offer">
+
+    <div class="col--left">
+      <p> {{ data.name }} </p>
+      <span> {{ data.hash }} </span>
+    </div>
+
+    <div class="col--right">
+      <nuxt-link 
+        to="/jobs/" 
+        tag="a"> VIEW MORE </nuxt-link>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "JobOffer",
+    props: {
+      data: {
+        type: Object,
+        required: true,
+        default: function () {
+          return {
+            job: 'dev'
+          }
+        }
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss" scoped>
+
+  .jobs__list__offer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 3.3rem 0;
+  }
+
+  .jobs__list__offer:nth-child(odd) {
+    background: gray;
+    width: 144rem;
+    margin-left: -14.4rem;
+    padding-left: 14.4rem;
+  }
+
+  .col--right {
+    padding: 1.4rem 4.3rem;
+    background-color: white;
+    color: red;
+  }
+
+</style>
--- components/Jobs/Jobs.vue
+++ components/Jobs/Jobs.vue
@@ -6,15 +6,39 @@
     </h2>
 
     <p class="jobs-content">
-      We are always looking for motivated, multi-talented, and organized people to join our diverse team! So if you want to be part of our team, check out open positions and apply today at <span>posao@diwanee-serbia.com</span>
+      We are always looking for motivated, multi-talented, and organized people to join our diverse team! So if you
+      want to be part of our team, check out open positions and apply today at <span>posao@diwanee-serbia.com</span>
     </p>
+
+    <div class="jobs__list">
+      <job-offer  
+        v-for="(job,index) in jobList" 
+        :key="index" 
+        :data="job" />
+    </div>
 
   </article>
 </template>
 
 <script>
-export default {};
+  import JobOffer from "~/components/Jobs/JobOffer";
+
+  export default {
+    components: {
+      JobOffer
+    },
+    data() {
+      return {
+        jobList: [
+          { name: "PHP DEVELOPER", hash: "#PHP #SQL #GIT #OOP" },
+          { name: "JUNIOR PHP DEVELOPER", hash: "#PHP #SQL #GIT #OOP" },
+          { name: "WEB DESIGNER", hash: "#PHP #SQL #GIT #OOP" }
+        ] 
+      }
+    }
+  };
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 </style>

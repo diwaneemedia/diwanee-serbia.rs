83e092e1c7be987efed67a854d001575ded95b2c
_vasilie <vasilije.milenkovic@diwanee.com>
2018-10-09 13:01:52 +0200

    game fixes

--- components/Game/VasilyGame.vue
+++ components/Game/VasilyGame.vue
@@ -16,7 +16,7 @@ export default {
   methods: {
     UnityLoaderReady() {
       window.gameInstance = UnityLoader.instantiate("gameContainer", [
-        "Build/pong2018ver2.json"
+        "Build/pong2018ver3.json"
       ]);
     }
   },

f92d3c788f8308c0194304a94b86e2482e38c0b0
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-10-19 16:22:48 +0200

    newest config

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,22 +57,22 @@ module.exports = {
       {
         type: "text/javascript",
         id: "facebook-jssdk",
-        src: "//connect.facebook.net/en_US/sdk.js"
+        src: "https://connect.facebook.net/en_US/sdk.js"
       },
       {
         type: "text/javascript",
         async: true,
-        src: "//www.googletagmanager.com/gtm.js?id=GTM-5W2CLW"
+        src: "https://www.googletagmanager.com/gtm.js?id=GTM-5W2CLW"
       },
       {
         type: "text/javascript",
         async: true,
         src: "https://connect.facebook.net/signals/config/837271083056525?v=2.8.30&r=stable"
       },
-      {
-        type: "text/javascript",
-        content: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date.getTime(), event: 'gtm.js'});var f = d.getElementsByTagName(s)[0],j = d.createElement(s),dl = l != 'dataLayer' ? '&l=' + l : '';j.async = true;j.src ='//www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j, f);})(window, document, 'script', 'dataLayer', 'GTM-5W2CLW');"
-      }
+      // {
+      //   type: "text/javascript",
+      //   content: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date.getTime(), event: 'gtm.js'});var f = d.getElementsByTagName(s)[0],j = d.createElement(s),dl = l != 'dataLayer' ? '&l=' + l : '';j.async = true;j.src ='//www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j, f);})(window, document, 'script', 'dataLayer', 'GTM-5W2CLW');"
+      // }
     ],
   },
 

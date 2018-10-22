4bc1fc8445e907c0ec1d68617e2ab57f701c2a00
Wekios <diwanee@Bojans-MacBook-Pro.local>
2018-10-22 12:32:27 +0200

    deleted social for google and unused icons

--- components/UI/Social.vue
+++ components/UI/Social.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="social">
-
+    
+    <!-- facebook -->
     <a href="https://www.facebook.com/diwaneeserbia/" class="social__btn social__facebook" target="_blank">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3.883 8.333">
         <defs>
@@ -13,9 +14,9 @@
         <path d="M128.2 2.73v-.718a1.581 1.581 0 0 1 .015-.25.486.486 0 0 1 .068-.174.279.279 0 0 1 .172-.119 1.224 1.224 0 0 1 .316-.033h.718V0h-1.148a1.868 1.868 0 0 0-1.431.473 1.991 1.991 0 0 0-.435 1.393v.864h-.86v1.436h.86v4.167h1.725V4.166h1.148l.152-1.436z"
               class="cls-1" transform="translate(-125.619)" />
       </svg>
-      <!-- facebook -->
     </a>
-
+    
+    <!-- twitter -->
     <a href="https://twitter.com/diwaneeserbia" class="social__btn social__twitter" target="_blank">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9.968 8.097">
         <defs>
@@ -30,35 +31,6 @@
                 class="cls-1" data-name="Path 21" transform="translate(-.001 -57.441)" />
         </g>
       </svg>
-
-      <!-- twitter -->
-    </a>
-
-    <a href="https://plus.google.com/+Diwanee-serbiaRs" class="social__btn social__google" target="_blank">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.781 7.988">
-        <defs>
-          <style>
-            .cls-1 {
-            fill: #7e7e7e
-            }
-          </style>
-        </defs>
-        <g transform="translate(-761.545 -5995.545)">
-          <g id="Group_79" data-name="Group 79" transform="translate(770.332 5997.143)">
-            <g id="Group_78" data-name="Group 78">
-              <path id="Path_22" d="M354.4 161.6V160h-.8v1.6H352v.8h1.6v1.6h.8v-1.6h1.6v-.8z" class="cls-1" data-name="Path 22"
-                    transform="translate(-352 -160)" />
-            </g>
-          </g>
-          <g id="Group_81" data-name="Group 81" transform="translate(761.545 5995.545)">
-            <g id="Group_80" data-name="Group 80">
-              <path id="Path_23" d="M3.994 99.2v1.6h2.26a2.4 2.4 0 1 1-2.26-3.2 2.361 2.361 0 0 1 1.552.578l1.05-1.2A3.958 3.958 0 0 0 3.994 96a3.994 3.994 0 1 0 3.994 3.994v-.8z"
-                    class="cls-1" data-name="Path 23" transform="translate(0 -96)" />
-            </g>
-          </g>
-        </g>
-      </svg>
-      <!-- google -->
     </a>
 
   </div>
